fdee0ed58a6a3927d1156f207a1a808a3250316f
Your Name <you@example.com>
2024-06-13 02:18:26 +0800

    .

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,16 +5,42 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ Auth::user()->usertype == 'admin' ? route('admin.dashboard') : route('dashboard')}}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="Auth::user()->usertype == 'admin' ? route('admin.dashboard') : route('dashboard')" :active="Auth::user()->usertype == 'admin' ? request()->routeIs('admin.dashboard') : request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    {{-- admin link --}}
+                    @if (Auth::user()->usertype == 'admin')
+                        
+                    <x-nav-link href="admin/club" :active="request()->routeIs('admin.club')">
+                        {{ __('Club') }}
+                    </x-nav-link>
+
+                    <x-nav-link href="admin/application" :active="request()->routeIs('admin.application')">
+                        {{ __('Application') }}
+                    </x-nav-link>
+
+                    <x-nav-link href="admin/event" :active="request()->routeIs('admin.event')">
+                        {{ __('Event') }}
+                    </x-nav-link>
+
+                    <x-nav-link href="admin/user" :active="request()->routeIs('admin.user')">
+                        {{ __('User') }}
+                    </x-nav-link>
+
+
+                    @endif
+
+                    {{-- user link --}}
+
+
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -18,3 +16,15 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth','userMiddleware'])->group(function(){
+
+    Route::get ('dashboard',[UserController::class,'index'])->name('dashboard');
+
+});
+
+Route::middleware(['auth','adminMiddleware'])->group(function(){
+
+    Route::get ('/admin/dashboard',[AdminController::class,'index'])->name('admin.dashboard');
+
+});
